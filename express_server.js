c143760a0ca4340adcd55047ab74ac2a916199b8
Richard Miao <richardmiao1645@gmail.com>
2022-10-26 13:18:06 -0700

    Use ejs to create a database for URLs

--- express_server.js
+++ express_server.js
@@ -13,9 +13,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
+/*
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+*/
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
